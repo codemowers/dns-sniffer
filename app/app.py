cc5ee5a5e37e268554e3f35ab8600f3b8da16716
Lauri Võsandi <lauri@k-space.ee>
2023-04-16 08:15:29 +0300

    Make application configurable via command line arguments

--- app/app.py
+++ app/app.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
+import argparse
 from cachetools import LRUCache
 from sanic import Sanic
 from sanic.response import json
 from prometheus_client import Gauge
 from sanic_prometheus import monitor
 from scapy.all import AsyncSniffer, DNS
+
+parser = argparse.ArgumentParser(
+    prog="DNS sniffer",
+    description="Record DNS responses and build up reverse lookup table")
+parser.add_argument("-f", "--filter",
+    default="inbound and port 53",
+    help="Specify packet capture filter")
+parser.add_argument("-i", "--interface",
+    help="Specify interace for capturing packets")
+parser.add_argument("-v", "--verbose",
+    action="store_true",
+    help="Log recorded responses")
+args = parser.parse_args()
 
 gauge_reverse_records = Gauge("dns_sniffer_record_count",
     "Reverse lookup table record count")
@@ -35,14 +49,16 @@ def process_packet(p):
         hostname = query.decode("ascii").lower().rstrip(".")
         if hostname.endswith(".local"):
             continue
+        if args.verbose:
+            print("%s: %s" % (answer, hostname))
         reverse_lookup[answer] = hostname
         gauge_reverse_records.set(len(reverse_lookup))
 
 
 @app.listener("before_server_start")
 async def setup_db(app, loop):
-    AsyncSniffer(prn=process_packet, filter="outbound and port 53", store=False).start()
-    AsyncSniffer(iface="lo", prn=process_packet, filter="outbound and port 53", store=False).start()
+    AsyncSniffer(prn=process_packet, filter=args.filter, store=False,
+        iface=args.interface).start()
 
 
 if __name__ == "__main__":
